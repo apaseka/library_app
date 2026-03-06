95d4d3f24d78826049264f5c2058be33f38d9a28
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-06 14:40:49 +0100

    refactor(book-service): clean up mapping and DTO usage

--- backend/src/main/java/com/example/library/service/BookService.java
+++ backend/src/main/java/com/example/library/service/BookService.java
@@ -7,13 +7,16 @@ import com.example.library.exception.ResourceNotFoundException;
 import com.example.library.mapper.BookMapper;
 import com.example.library.repository.AuthorRepository;
 import com.example.library.repository.BookRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BookService {
 
     private final BookRepository bookRepository;
@@ -26,13 +29,44 @@ public class BookService {
                 .toList();
     }
 
+    public BookDTO getById(Long id) {
+        Book book = getBookByIdOrThrow(id);
+        return BookMapper.toDto(book);
+    }
+
+    private @NonNull Book getBookByIdOrThrow(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        String.format("Book with id %d not found", id)
+                ));
+    }
+
     public BookDTO create(BookDTO dto) {
 
         Author author = findAuthorOrThrow(dto.getAuthor());
         Book book = BookMapper.toEntity(dto, author);
+
         Book savedBook = bookRepository.save(book);
 
-        return BookMapper.toDto(savedBook);
+        return BookMapper.toDto(book);
+    }
+
+    @Transactional
+    public BookDTO update(Long id, BookDTO dto) {
+        Book book = getBookByIdOrThrow(id);
+        Author author = findAuthorOrThrow(dto.getAuthor());
+
+        book.setYear(dto.getYear());
+        book.setTitle(dto.getTitle());
+        book.setAuthor(author);
+
+        /*
+        No need to call save() here. The entity is managed by JPA inside the transaction,
+        so Hibernate will automatically persist changes (dirty checking).
+        Book savedBook = bookRepository.save(book);
+         */
+
+        return BookMapper.toDto(book);
     }
 
     private Author findAuthorOrThrow(String authorName) {
@@ -40,5 +74,10 @@ public class BookService {
                 .orElseThrow(() -> new ResourceNotFoundException(
                         String.format("Author \"%s\" not found", authorName)
                 ));
+    }
+
+    public void deleteById(Long id) {
+        Book book = getBookByIdOrThrow(id);
+        bookRepository.delete(book);
     }
 }
