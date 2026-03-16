3d43201bb9adfd6eeba35654007a599697228db2
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-16 09:15:23 +0100

    chore: added toString method

--- backend/src/main/java/com/example/library/dto/response/AuthorDTO.java
+++ backend/src/main/java/com/example/library/dto/response/AuthorDTO.java
@@ -1,14 +1,12 @@
 package com.example.library.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class AuthorDTO {
     private Long id;
     private String name;
--- backend/src/main/java/com/example/library/dto/response/BookDTO.java
+++ backend/src/main/java/com/example/library/dto/response/BookDTO.java
@@ -1,14 +1,12 @@
 package com.example.library.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class BookDTO {
     private Long id;
     private String title;
