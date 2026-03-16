110d2c37bfd49119950315de39d80ef02925e031
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-16 09:18:12 +0100

    refactor: renamed class due to usage

--- backend/src/main/java/com/example/library/dto/request/CreateUpdateBookRequest.java
+++ backend/src/main/java/com/example/library/dto/request/CreateUpdateBookRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record CreateBookRequest(
+public record CreateUpdateBookRequest(
 
         @NotBlank(message = "Title must not be blank")
         @Size(max = 200, message = "Title must be shorter than 200 characters")
