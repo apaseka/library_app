9c22216d37ddf332153d89c92218339246f7fe99
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-16 09:24:48 +0100

    refactor: used CreateUpdateBookRequest

--- backend/src/main/java/com/example/library/service/BookService.java
+++ backend/src/main/java/com/example/library/service/BookService.java
@@ -1,12 +1,11 @@
 package com.example.library.service;
 
-import com.example.library.dto.request.CreateBookRequest;
+import com.example.library.dto.request.CreateUpdateBookRequest;
 import com.example.library.dto.response.BookDTO;
 import com.example.library.entity.Author;
 import com.example.library.entity.Book;
 import com.example.library.exception.ResourceNotFoundException;
 import com.example.library.mapper.BookMapper;
-import com.example.library.repository.AuthorRepository;
 import com.example.library.repository.BookRepository;
 import com.example.library.util.AuthorHelper;
 import lombok.NonNull;
@@ -22,7 +21,6 @@ import java.util.List;
 public class BookService {
 
     private final BookRepository bookRepository;
-    private final AuthorRepository authorRepository;
     private final AuthorHelper authorHelper;
 
     public List<BookDTO> getAll() {
@@ -37,15 +35,8 @@ public class BookService {
         return BookMapper.toDto(book);
     }
 
-    private @NonNull Book getBookByIdOrThrow(Long id) {
-        return bookRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException(
-                        String.format("Book with id %d not found", id)
-                ));
-    }
-
     @Transactional
-    public BookDTO create(CreateBookRequest request) {
+    public BookDTO create(CreateUpdateBookRequest request) {
 
         Author author = authorHelper.getAuthorIfExists(request.author());
         Book book = BookMapper.toEntity(request, author);
@@ -54,26 +45,28 @@ public class BookService {
     }
 
     @Transactional
-    public BookDTO update(Long id, BookDTO dto) {
+    public BookDTO update(Long id, CreateUpdateBookRequest request) {
         Book book = getBookByIdOrThrow(id);
-        Author author = authorHelper.getAuthorIfExists(dto.getAuthor());
+        Author author = authorHelper.getAuthorIfExists(request.author());
 
-        book.setYear(dto.getYear());
-        book.setTitle(dto.getTitle());
+        book.setYear(request.year());
+        book.setTitle(request.title());
         book.setAuthor(author);
 
-        /*
-        No need to call save() here. The entity is managed by JPA inside the transaction,
-        so Hibernate will automatically persist changes (dirty checking).
         Book savedBook = bookRepository.save(book);
-         */
-
-        return BookMapper.toDto(book);
+        return BookMapper.toDto(savedBook);
     }
 
     @Transactional
     public void deleteById(Long id) {
         Book book = getBookByIdOrThrow(id);
         bookRepository.delete(book);
+    }
+
+    private @NonNull Book getBookByIdOrThrow(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        String.format("Book with id %d not found", id)
+                ));
     }
 }
