2cdc0d5431a8a2d90f3cdadf7beb7d069093fbd6
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-16 18:44:47 +0100

    feat: add Flyway database migrations. tuned tests

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 
 	compileOnly("org.projectlombok:lombok")
 
