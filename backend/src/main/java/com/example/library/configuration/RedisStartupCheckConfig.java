d9e1c9ce48f920e381752633a240d8043eb50a93
Oleksandr Pasieka <apasekakiev@gmail.com>
2026-03-28 11:10:54 +0100

    feat: added redis config to stop running app if redis is down

---
+++ backend/src/main/java/com/example/library/configuration/RedisStartupCheckConfig.java
@@ -0,0 +1,34 @@
+package com.example.library.configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Profile({"prod", "uat"})
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RedisStartupCheckConfig {
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public ApplicationRunner redisStartupCheck() {
+        return args -> {
+            try (var connection = redisConnectionFactory.getConnection()) {
+                String pong = connection.ping();
+                if (!"PONG".equalsIgnoreCase(pong)) {
+                    throw new IllegalStateException("Redis ping failed");
+                }
+                log.info("Redis is available");
+            } catch (Exception ex) {
+                log.error("Redis is NOT available", ex);
+                throw new IllegalStateException("Redis is required, but not available", ex);
+            }
+        };
+    }
+}
